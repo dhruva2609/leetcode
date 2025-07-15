53193442d2f8632688512507950abce527e06533
Dhruva Pandya <pandyadhruva09@gmail.com>
2025-07-15 07:47:56 +0530

    Add files via upload

---
+++ 3136_Valid_Word.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.length() < 3) return false;
+
+        bool hasVowel = false;
+        bool hasConsonant = false;
+
+        for (char c : word) {
+            if (!isalnum(c)) return false; // not a digit or letter
+
+            if (isalpha(c)) {
+                char lower = tolower(c);
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+};
