0ecdd23125992c02e8d9e6e3245b840221a58f88
Dhruva Pandya <pandyadhruva09@gmail.com>
2025-07-20 11:03:22 +0530

    Add files via upload

---
+++ 225_Implement_Stack_using_Queues.cpp
@@ -0,0 +1,36 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q1, q2;
+
+public:
+    MyStack() { }
+
+    void push(int x) {
+        q2.push(x);
+        // Move all elements from q1 to q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        // Swap q1 and q2
+        swap(q1, q2);
+    }
+
+    int pop() {
+        int topElement = q1.front();
+        q1.pop();
+        return topElement;
+    }
+
+    int top() {
+        return q1.front();
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
+
