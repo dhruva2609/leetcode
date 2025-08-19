82f23d6b67e5782c675618364809f5ddc600325b
Dhruva Pandya <pandyadhruva09@gmail.com>
2025-08-19 13:19:37 +0530

    Add files via upload

---
+++ 2348_Number_of_Zero-Filled_Subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0, count = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                count++;
+                result += count;
+            } else {
+                count = 0;
+            }
+        }
+        return result;
+    }
+};
